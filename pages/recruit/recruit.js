bb5e4a452198b48038e79198078eee66676975f2
xiari <836359170@qq.com>
2020-05-20 01:51:25 +0800

    招募页面
    招募页面静态页面

---
+++ pages/recruit/recruit.js
@@ -0,0 +1,103 @@
+// pages/recruit/recruit.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  data: {
+    is_official : true, 
+    official_recruits:[
+      {
+        urgent : true,
+        head : "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588314029275&di=763dff5ec910ce77155cf285e9da125f&imgtype=0&\src=http%3A%2F%2Fa0.att.hudong.com%2F64%2F76%2F20300001349415131407760417677.jpg",
+        name : "很神奇的凑字数的南大团委",
+        work : "新年音乐会场务",
+        time : "12月30日 19：00 - 22：00",
+        place : "仙林校区大活",
+        reword : "20元饭票",
+        tel : "12345677899",
+        more : "搬搬道具，听现场指挥就行"
+      },
+      {
+        urgent : false,
+        head : "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588321392410&di=808f39e737ae40b465d6203f649d56bd&imgtype=0&src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_match%2F0%2F7405829970%2F0",
+        name : "街舞社",
+        work : "找人录像",
+        time : "12月30日-12月31日 19：00 - 22：00",
+        place : "四五六食堂门前空地",
+        reword : "没有",
+        tel : "12345677899",
+        more : "帮忙录个像，很简单的！"
+      },
+      {
+        urgent : true,
+        head : "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588314029275&di=763dff5ec910ce77155cf285e9da125f&imgtype=0&\src=http%3A%2F%2Fa0.att.hudong.com%2F64%2F76%2F20300001349415131407760417677.jpg",
+        name : "很神奇的凑字数的南大团委",
+        work : "音乐作品征集比赛海报设计",
+        time : "5月25号前完成",
+        place : "无",
+        reword : "100-200元",
+        tel : "12345677899",
+        more : "我们会在报名者中筛选合适的设计师，请报名者尽快将个人作品发送至邮箱181250001@smail.nju.edu.cn，我们明天会以短信形式通知入选者"
+      },
+    ],
+    non_official_recruits:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
